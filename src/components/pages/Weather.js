50eb16d032db237a863835973b455a508d0d45e0
irtiza07 <35472116+irtiza07@users.noreply.github.com>
2019-02-04 23:34:51 -0500

    Cleaned boilerplate. Laid out skeleton

---
+++ src/components/pages/Weather.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Weather extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the Weather Page</h1>
+      </div>
+    )
+  }
+}
+
+export default Weather
